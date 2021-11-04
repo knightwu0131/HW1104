a97070447284a75a170f61432139997c2195a8a5
knightwu <zxc7365265@gmail.com>
2021-11-04 21:39:56 +0800

    hw

---
+++ test_p15/src/test_p15.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test_p15 {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("請輸入整數");
+	     BufferedReader br=
+	    		 new BufferedReader(new InputStreamReader(System.in));
+	     int num =Integer.parseInt(br.readLine());
+	     
+	     switch(num) {
+	     case 1:
+	    	 System.out.println("輸入的是1");
+	     case 2:
+	    	 System.out.println("輸入的是2");
+
+	      case 3:
+	    	 System.out.println("請輸入1或2");
+		}
+
+	}
+	
+}
---
+++ test_p42/src/test_p42.java
@@ -0,0 +1,14 @@
+
+public class test_p42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub3
+		int[]test= {80,60,22,50,75};
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("第"+(i+1)+"個人分數是"+test[i]+"分");
+		}
+
+  }
+}
